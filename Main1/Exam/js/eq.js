d06d5d68f5f1903e62dc8c6730949b0e30828540
Chem-World <71706368+Chem-World@users.noreply.github.com>
2020-12-27 03:04:49 +0530

    Update eq.js

--- Main1/Exam/js/eq.js
+++ Main1/Exam/js/eq.js
@@ -367,12 +367,12 @@ function initQuiz() {
     $("#setQuizSize").text('Set number of quesitons in quiz');
     $("#fetchingQ").text('Loading questions from database..');
     document.body.style = "text-align:right;unicode-bidi:bidi-override; direction:rtl;"
-    rootRef = fb.database().ref('question').child('Equilibrium');
+    rootRef = fb.database().ref('question').child('Sol');
     loadAllQuestions();
 }
 /************************************* MAIN **********************************************************/
 
-var qInQuiz = 7;       // default
+var qInQuiz = 1;       // default
 var nextQuestionText, correctText, qInDB;
 var checkAnswer, questionText, notEnoughQuestionMessage;
 var questionTextOf, doneQuizText1, doneQuizText2, doneQuizText3, doneQuizText4;
